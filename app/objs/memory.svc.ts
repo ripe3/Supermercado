c0acc60a4a64c74338d4c6ed3bea1e31e335eca0
ripe3 <ripe3@users.noreply.github.com>
2017-10-12 14:50:55 -0300

    License header fix

--- app/objs/memory.svc.ts
+++ app/objs/memory.svc.ts
@@ -1,17 +1,15 @@
 /*
- * Copyright 2016 ripe3 - https://github.com/ripe3
+ * Supermercado <https://github.com/ripe3/Supermercado>
+ * 
+ * Copyright (C) 2016-2017 - Luis Filipe <https://github.com/ripe3>
+ * 
+ * This program is free software under the terms of the 
+ * GNU General Public License 3 (GPLv3) as published by
+ * the Free Software Foundation.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
  */
 
 import { InMemoryDbService } from 'angular2-in-memory-web-api';
